b2cbf850841cd9c51fb452e374c997e1d727cfc6
Jinmark-kai <2268128377@qq.com>
2023-03-20 22:38:07 +0800

    site配置设置

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { remarkReadingTime } from './src/scripts/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
+    site: "http://localhost:3000/",
     markdown: {
         shikiConfig: {
             theme: "github-dark",
