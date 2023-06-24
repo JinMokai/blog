d3bc75903664a08a958a8747a634a4747fae79ae
Jinmark-kai <2268128377@qq.com>
2023-06-25 07:58:38 +0800

    fix:html的lang,add:robots and article,update:字体更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import { remarkReadingTime } from './src/scripts/remark-reading-time.mjs';
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://chkaiblog.eu.org/",
-    markdown: {
-        shikiConfig: {
-            theme: "dracula",
-        },
-        syntaxHighlight: 'shiki',
-        rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
-            behavior: 'wrap'
-        }]],
-        remarkPlugins: [remarkReadingTime],
-        extendDefaultPlugins: true
+  site: "https://chkaiblog.eu.org/",
+  markdown: {
+    shikiConfig: {
+      theme: "dracula"
     },
-    integrations: [sitemap()]
+    syntaxHighlight: 'shiki',
+    rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
+      behavior: 'wrap'
+    }]],
+    remarkPlugins: [remarkReadingTime],
+    extendDefaultPlugins: true
+  },
+  integrations: [sitemap(), robotsTxt()]
 });
